39324796ac6172a307e58079e7c7a8f0bca447c6
bhuwan <bhuwankashyap5@gmail.com>
2025-07-22 22:17:11 +0530

    Encapsulation is studied

---
+++ Encapsulation/Human.java
@@ -0,0 +1,31 @@
+
+
+public class Human {
+    public static  class Student{
+        private String name;
+        private int age;
+
+        public Student(String name,int age){
+            this.name=name;
+            this.age=age;
+        }
+
+        public void setname(String name){
+            this.name=name;
+        }
+        public void setage(int age){
+            this.age=age;
+        }
+    }
+    public static void main(String[] args) {
+        // Encapsulation(Hiding the Data so that we can use set and get )
+        Student h1=new Student("bhuwan",100);
+        // h1.name="bhuwan";
+        // h1.age=100; these 2 lines are invalid as they are private filed
+        h1.setage(1000);
+
+        
+        
+    }
+    
+}
